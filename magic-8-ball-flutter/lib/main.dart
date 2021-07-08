ad7ab6802499de169a3672d3d00ba1b18dab36ec
henrylong <henrylong719@gmail.com>
2021-07-08 20:48:32 +0930

    fix

---
+++ magic-8-ball-flutter/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'Dart:math';
+
+void main() => runApp(
+      MaterialApp(home: BallPage()),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      ballNumber = Random().nextInt(5) + 1;
+                    });
+                  },
+                  child: Image.asset('images/ball$ballNumber.png')))
+        ],
+      ),
+    );
+  }
+}
