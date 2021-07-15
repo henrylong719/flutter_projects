506698211782e41c97240ab06e4af8ed00ff9cdf
henrylong <henrylong719@gmail.com>
2021-07-15 20:59:12 +0930

    add new

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable_card.dart';
+import 'ion_content.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
+const inactiveCardColour = Color(0xFF111328);
 const bottomContainerColour = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -10,6 +16,7 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,8 +28,36 @@ class _InputPageState extends State<InputPage> {
             Expanded(
               child: Row(
                 children: [
-                  Expanded(child: ReusableCard(colour: activeCardColour)),
-                  Expanded(child: ReusableCard(colour: activeCardColour)),
+                  Expanded(
+                      child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.male;
+                      });
+                    },
+                    colour: selectedGender == Gender.male
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                  )),
+                  Expanded(
+                      child: ReusableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    colour: selectedGender == Gender.female
+                        ? activeCardColour
+                        : inactiveCardColour,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
+                    ),
+                  )),
                 ],
               ),
             ),
@@ -49,19 +84,5 @@ class _InputPageState extends State<InputPage> {
             )
           ],
         ));
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour});
-
-  final Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        margin: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
-            color: colour, borderRadius: BorderRadius.circular(10.0)));
   }
 }
---
+++ bmi-calculator-flutter/lib/ion_content.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const labelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
+
+class IconContent extends StatelessWidget {
+  IconContent({@required this.icon, @required this.label});
+
+  final IconData icon;
+  final String label;
+
+  // FontAwesomeIcons.mars
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, size: 80.0),
+        SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          label,
+          style: labelTextStyle,
+        )
+      ],
+    );
+  }
+}
