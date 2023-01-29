2532832b3b0ab0fff578523b96cc7b4e0ea2d134
henrylong719 <henrylong719@gmail.com>
2023-01-30 09:05:21 +1030

    add show favorite item

--- shop_app/lib/providers/providers.dart
+++ shop_app/lib/providers/providers.dart
@@ -37,12 +37,30 @@ class Products with ChangeNotifier {
     ),
   ];
 
+  var _showFavoritesOnly = false;
+
   List<Product> get items {
+    if (_showFavoritesOnly) {
+      return _items.where((prodItem) => prodItem.isFavorite).toList();
+    }
+
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
     return _items.firstWhere((prod) => prod.id == id);
+  }
+
+  void showFavoritesOnly() {
+    _showFavoritesOnly = true;
+  }
+
+  void showAll() {
+    _showFavoritesOnly = false;
   }
 
   void addProduct() {
--- shop_app/lib/widgets/products_grid.dart
+++ shop_app/lib/widgets/products_grid.dart
@@ -5,10 +5,14 @@ import 'package:provider/provider.dart';
 import '../providers/providers.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
 
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
